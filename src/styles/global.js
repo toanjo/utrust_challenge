cb9136b17a305ff02202f316dd53079075be33cf
António Anjo <to.anjo57@gmail.com>
2019-11-16 14:12:36 +0000

    fixed disabled button style

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,15 +1,34 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  main {
+    margin: auto;
+  }
+
   .App {
     display: flex;
     height: 100vh;
+  }
+
+  .card {
+    width: 580px; 
+    min-height: 325px;
+    border: 1px solid rgba(105, 50, 212, 0.2);
+    box-shadow: 0px 8px 24px rgba(32, 37, 50, 0.15);
+    border-radius: 8px;
   }
 
   .button {
     color: white;
     background-color: #6932D4;
     border-radius: 100px;
+
+    /* base button styling copied from Bulma CSS */
     cursor: pointer;
     justify-content: center;
     padding-bottom: calc(.5em - 1px);
@@ -42,22 +61,7 @@ export default createGlobalStyle`
   }
 
   .button:disabled {
-    background: #6932D4;
-  }
-
-  .card {
-    width: 580px; 
-    min-height: 325px;
-    border: 1px solid rgba(105, 50, 212, 0.2);
-    box-shadow: 0px 8px 24px rgba(32, 37, 50, 0.15);
-    border-radius: 8px;
-  }
-
-  main {
-    margin: auto;
-  }
-
-  body {
-    font-family: 'Roboto', sans-serif;
+    background: #A180E5;
+    cursor: not-allowed;
   }
 `
